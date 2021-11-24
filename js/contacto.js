805dd8c6313cfcedc5b2b596aba9d1d0e0f1a0e3
Gustavo Grosso <gustavodanielgrosso@gmail.com>
2021-11-24 16:33:19 -0300

    Validación de Form

--- js/contacto.js
+++ js/contacto.js
@@ -1,48 +1,27 @@
 
-$("#validarNombre").change((e) => {
-    if (e.target.value.lenght < 2) {
-        $("#validarnombre").addClass("errorValidacion");
+
+let nombreOk = false;
+let mailOk = false;
+
+function enviarFormulario (){
+    let mensajeError = [];
+    if ($("#nombre").val() === null || $("#nombre").val() === "") {
+        mensajeError.push("Falta ingresar tu nombre");
     }
-    else {
-        $("#validarnombre").removeClass("errorValidacion");
+    else{
+        nombreOk = true;
     }
+
+    if ($("#mail").val() === null || $("#mail").val() === "") {
+        mensajeError.push("Falta ingresar tu mail");
+    }
+    else{
+        mailOk = true;
+    }
+    $("#error").append(mensajeError.join(", "));
+    if (nombreOk && mailOk){
+        $("#error").append('<p class="envioCorrecto"> Formulario enviado con éxito </p>');
+    }
+    return false;
 }
-)
-
-
-
-
-
-
-
-/*
-$("#formulario").submit(function (e) {
-    e.preventDefault();
-    if ($("#validarNombre" === "")) {
-        alert("Debe completar Obligatoriamente un nombre");
-    }
-    else if ($("#validarMail" === "")) {
-        alert("Debe completar Obligatoriamente un mail");
-    }
-    return true;
-})
-*/
-
-
-
-
-
-/*
-
-    if ($("#validarNombre" == "")) {
-        alert("Debe completar Obligatoriamente un nombre");
-        return false;
-    }
-    if ($("#validarMail" == "")) {
-        alert("Debe completar Obligatoriamente un mail");
-        return false;
-    }
-    return true;
-})
-*/
 
